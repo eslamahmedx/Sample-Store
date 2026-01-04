f9e2a194664e7da38008db5ae62aaeb1ab1432a8
Eslam Ahmed <solomice123@gmail.com>
2026-01-04 04:05:12 +0200

    Uncomment HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter } from "react-router-dom";      /
+import { HashRouter } from "react-router-dom"; 
 import App from "./App";
 import "./styles/products.css";
 
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <HashRouter> {}
+    <HashRouter>
       <App />
     </HashRouter>
   </QueryClientProvider>
