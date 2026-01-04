cbf62b66967c877ebfe9bb515638ba3e894d2895
Eslam Ahmed <solomice123@gmail.com>
2026-01-04 04:22:04 +0200

    Refactor main.jsx to remove HashRouter
    Removed HashRouter wrapper around App component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter } from "react-router-dom"; 
 import App from "./App";
 import "./styles/products.css";
 
@@ -9,8 +8,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </QueryClientProvider>
 );
